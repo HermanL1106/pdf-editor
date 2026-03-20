9dcff48cf4cc7844fd93e226fd8930689fcab44a
Herman <herman@example.com>
2026-03-20 14:25:19 +0800

    Add GitHub Pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/pdf-editor/',
   optimizeDeps: {
     include: ['pdfjs-dist']
+  },
+  build: {
+    outDir: 'docs'
   }
 })
